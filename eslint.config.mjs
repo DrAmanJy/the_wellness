ed832cf3494ac7fabb637ca28bcc0355cf62c40b
DrAmanJy <dramanjy0770@gmail.com>
2026-08-26 15:57:51 +0530

    build: configure strict typescript and eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import prettierConfig from 'eslint-config-prettier';
-import importPlugin from 'eslint-plugin-import';
+import importPlugin from 'eslint-plugin-import-x';
 import unusedImportsPlugin from 'eslint-plugin-unused-imports';
 
 export default tseslint.config(
